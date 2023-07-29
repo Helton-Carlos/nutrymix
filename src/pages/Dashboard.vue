65fb0fbad0a9f6d0aa50372526956f90faf57352
Helton Brito <heltoncarlospa@hotmail.com>
2023-07-28 23:19:00 -0300

    feat: add draggable in card

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import CardDraggable from '../components/CardDraggable.vue'
+  import { ref, computed } from 'vue'
+  import CardDraggable from '@/components/CardDraggable.vue'
+  import Card from '@/components//Card.vue'
+  import draggable from 'vuedraggable'
+
+  const drag = ref<boolean>(false)
+
+  const dragOptions = computed(() => {
+    return {
+      animation: 200,
+      group: 'description',
+      disabled: false,
+      ghostClass: 'ghost',
+    }
+  })
 
   interface ICards {
     client: string
@@ -8,7 +21,7 @@
     hour: string
   }
 
-  const Cards = ref<ICards[]>([
+  let cards = ref<ICards[]>([
     { client: 'Renato Carlos', type: 'dieta', hour: '09:00' },
     { client: 'João Brito', type: 'renal', hour: '10:00' },
     { client: 'Jose Brito', type: 'diabetico', hour: '11:00' },
@@ -21,6 +34,40 @@
 
 <template>
   <div>
-    <CardDraggable :cards="Cards" />
+    <CardDraggable>
+      <template #body>
+        <draggable
+          class="list-group"
+          tag="transition-group"
+          :component-data="{
+            tag: 'ul',
+            type: 'transition-group',
+            name: !drag ? 'flip-list' : null,
+          }"
+          v-model="cards"
+          v-bind="dragOptions"
+          @start="drag = true"
+          @end="drag = false"
+          item-key="order"
+        >
+          <template #item="{ element }">
+            <li class="list-group-item">
+              <i
+                :class="
+                  element.fixed ? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'
+                "
+                @click="element.fixed = !element.fixed"
+                aria-hidden="true"
+              ></i>
+              <Card
+                :client="element.client"
+                :type="element.type"
+                :hour="element.hour"
+              />
+            </li>
+          </template>
+        </draggable>
+      </template>
+    </CardDraggable>
   </div>
 </template>
