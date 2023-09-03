bde4b100e421c8cb5eb6cee82d31cabc48aa4bf8
Helton Brito <heltoncarlospa@hotmail.com>
2023-09-03 15:12:20 -0300

    feat: add css fade active

--- src/layout/Sidebar.vue
+++ src/layout/Sidebar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { RouterLink } from 'vue-router'
-  import { breakpoints } from '@/use/breakpoints.use'
+  import { breakpoints } from '../use/breakpoints.use'
 
   defineProps<{ menuMobile: boolean }>()
 
@@ -27,28 +27,34 @@
       </div>
     </div>
   </div>
-
-  <div
-    class="absolute z-10 top-0 right-0 min-w-full min-h-screen bg-secondary bg-opacity-75 transition-opacity"
-    v-if="xs && menuMobile"
-  >
-    <div class="sidebar-mobile">
-      <div class="text-left py-8 mt-4">
-        <div v-for="routes in router" :key="routes.name">
-          <router-link :to="routes.path" @click="$emit('closeMenu')">
-            <i :class="`cursor-pointer pr-2 my-4 pi pi-${routes.icon}`" />
-            <span class="capitalize font-semibold">{{
-              routes.name
-            }}</span></router-link
-          >
+  <transition name="fade" mode="out-in">
+    <div
+      class="absolute z-10 top-0 right-0 min-w-full min-h-screen bg-secondary bg-opacity-75 transition-opacity"
+      v-if="xs && menuMobile"
+    >
+      <div class="sidebar-mobile">
+        <div class="text-left py-8 mt-4">
+          <div v-for="routes in router" :key="routes.name">
+            <router-link :to="routes.path" @click="$emit('closeMenu')">
+              <i :class="`cursor-pointer pr-2 my-4 pi pi-${routes.icon}`" />
+              <span class="capitalize font-semibold">{{
+                routes.name
+              }}</span></router-link
+            >
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <style scoped>
   .router-link-active {
     @apply bg-primary text-white py-1 px-1.5 rounded-md transition-colors;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all 0.3s ease-out;
   }
 </style>
