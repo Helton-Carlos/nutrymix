b290ebe47728f65076e33a4b994d50ede82c3d0b
Helton Silva <helton.silva@melhorenvio.com>
2023-09-14 14:34:37 -0300

    feat: add images

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,54 @@ import { VitePWA } from 'vite-plugin-pwa'
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue(), VitePWA()],
+  plugins: [
+    vue(), 
+    VitePWA({
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,png,svg,mp3}',
+        ],
+      },
+      manifest: { 
+        theme_color: '#5278FF',
+        background_color: '#FFF',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        name: 'Nutrymix',
+        short_name: 'Nutrymix',
+        description: 'Nutrymix projeto CRM para de nutricionista. Vue3, Vite, Typescript, Vitest e Tailwind CSS e bibliotecas adicionais.',
+        lang: 'pt-BR',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-32x32.png',
+            sizes: '32x32',  
+            type: 'image/png',
+          },
+        ],
+      }
+    })
+],
   resolve: {
     alias: [
       { find: "@/", replacement: "/src" },
